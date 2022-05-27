abc4db7214423337d53bb86ea6de4a4c0e040b3b
root <chu1231104@gmail.com>
2022-05-27 07:43:33 +0000

    finish register

--- backend/api/Register.py
+++ backend/api/Register.py
@@ -16,24 +16,20 @@ def register():
     """  
     連接DB, 更新目前的status
     """
-    table = get_bigtable('auth')
-    row_filter = row_filters.CellsColumnLimitFilter(1)
-    rows = table.read_rows(filter_=row_filter)
-    user_list = [{'user': r.row_key, 'password': r.cells['information']['password'][0]} for r in rows]
-
     data = json.loads(request.data.decode('utf-8'))
-    print(data)
-    print(type(data))
-    for d in user_list:
-        if d["user"] == data["user"]:
-            return {"result": "repeat"}
+    table = get_bigtable('auth')
+
+    # "already_annotated" will inject the bigtable cell, thus prohibit
     if data["user"] == "already_annotated":
         return {"result": "prohibited username"}
-    
-    timestamp = datetime.utcnow()
-    row_key = data["user"]
-    row = table.direct_row(row_key)
-    row.set_cell('information', 'password', data['password'], timestamp)
-    row.commit()
 
-    return {"result": "Ok"}
+    test_row = table.read_row(data['user'])
+    if test_row:
+        return {"result": "repeat"}
+    else:
+        timestamp = datetime.utcnow()
+        row_key = data["user"]
+        row = table.direct_row(row_key)
+        row.set_cell('information', 'password', data['password'], timestamp)
+        row.commit()
+        return {"result": "Ok"}
